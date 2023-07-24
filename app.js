15cf6d77dcdc1f311e850713e3f337bb1a3f6624
williams bolu <williams.bolu99gmail.com>
2023-07-24 09:52:08 +0100

    credentials test3

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ app.set('views', path.join(__dirname, 'views')); // define the views "pug" folde
 
 // (1) GLOBALS MIDDLEWARES
 // Implement CORS // for allowing everyone to consume our api // // Allow-Control-Allow-Origin
-// app.use(
-//     cors({
-//         origin: 'https://natours-react-three.vercel.app',
-//         credentials: true,
-//     })
-// );
+// app.use(cors())
 
 // "Only" allow this url below to interact with our api
-app.use(cors());
+app.use(
+    cors({
+        origin: ['https://natours-react-three.vercel.app'],
+        credentials: true,
+    })
+);
 
 // for non-simple request(put, patch, delete, cookie request)
 app.options('*', cors());
